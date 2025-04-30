bfea59bbb1bedf7599cb523ac2771fd4cf04bd5e
ANDRE-ASUS-PC\ANDRE_ABREU <andreluismachadoabreu@gmail.com>
2025-04-30 06:25:40 -0300

    Updating MobileNav component

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
     Sheet,
     SheetContent,
@@ -6,22 +8,67 @@ import {
     SheetTitle,
     SheetTrigger,
   } from "@/components/ui/sheet"
+import { sidebarLinks } from "@/constants"
+import { cn } from "@/lib/utils"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 const MobileNav = ({ user}: MobileNavProps ) => {
+  const pathname = usePathname();
   return (
-    <section>
+    <section className="w-full max-w-[264px]">
       <Sheet>
-  <SheetTrigger>Open</SheetTrigger>
-  <SheetContent>
-    <SheetHeader>
-      <SheetTitle>Are you absolutely sure?</SheetTitle>
-      <SheetDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </SheetDescription>
-    </SheetHeader>
-  </SheetContent>
-</Sheet>
+        <SheetTrigger>
+          <Image
+          src="/icons/hamburger.svg"
+          width={30}
+          height={30}
+          alt="menu"
+          className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-white">
+        <Link href="/" className="cursor-pointer flex items-center gap-1 px-4">
+            <Image
+                src="/icon/logo.svg"
+                width={34}
+                height={34}
+                alt="Horizon logo"
+            />
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+            Horizon
+            </h1>
+            </Link>
+
+            {sidebarLinks.map((item) => {
+
+                const isActive = 
+                pathname === item.route || pathname.startsWith(`${item.route}/`)
+
+                return (
+                    <Link href={item.route} key={item.label}
+                    className={cn('sidebar-link', {'bg-bank-gradient': isActive })}
+                    >
+                     <div className="relative size-6">
+                        <Image
+                          src={item.imgURL}
+                          alt={item.label}
+                          fill
+                          className={cn({
+                            'brightness-[3] invert-0': isActive
+                          })}
+                        />
+                     </div>
+                     <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                      {item.label}
+                     </p>
+                    </Link>
+                )
+            }
+        )}
+        </SheetContent>
+      </Sheet>
 
     </section>
   )
