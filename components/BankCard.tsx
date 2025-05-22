519c2ab9b8bfe9583bcc432988d30640be0a5190
Andre Abreu <andreluismachadoabreu@gmail.com>
2025-05-22 11:23:32 -0300

    Updating BankCard Component

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -6,6 +6,14 @@ true }: CreditCardProps) => {
   return (
     <div className="flex flex-col">
       <Link href="/" className="bank-card">
+        <div className="bank-card_content">
+          <div>
+            <h1 className="text-16 font-semibold
+             text-white">
+             {account.name || 'André'}
+            </h1>
+          </div>
+        </div>
       
       </Link>
     </div>
