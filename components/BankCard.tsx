0c6f09ba0fa9892c4a180594f71d22de82a94e66
ANDRE-ASUS-PC\ANDRE_ABREU <andreluismachadoabreu@gmail.com>
2025-05-29 00:18:11 -0300

    Updating BankCard component file

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,3 +1,4 @@
+import { formatAmount } from '@/lib/utils'
 import Link from 'next/link'
 import React from 'react'
 
@@ -9,9 +10,13 @@ true }: CreditCardProps) => {
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold
-             text-white">
-             {account.name || 'André'}
+           text-white">
+             {account.name || userName}
             </h1>
+            <p className="font-ibm-plex-serif font-black text-white">
+              {formatAmount(account.currentBalance)}
+
+            </p>
           </div>
         </div>
       
